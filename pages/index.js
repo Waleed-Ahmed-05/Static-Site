de847ae289866e47922ea43ae9ce42056d41e41d
Waleed-Ahmed-05 <LP.Gangster05@gmail.com>
2024-11-26 01:25:38 +0500

    1st commit

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,18 @@
-import path from 'path';
-import fs from 'fs';
-import matter from 'gray-matter';
-
-export default function Home({ postData }) {
-    return (
-        <div>
-            <h1>{postData.data.title}</h1>
-            <p>{postData.data.date}</p>
-            <div dangerouslySetInnerHTML={{ __html: postData.content }} />
-        </div>
-    );
-}
-
-export async function getStaticProps() {
-    const filePath = path.join(process.cwd(), 'posts', 'hello-world.md');
-    const fileContents = fs.readFileSync(filePath, 'utf8');
-    const { data, content } = matter(fileContents);
-
-    return {
-        props: {
-            postData: { data, content },
-        },
-    };
+import Head from "next/head";
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>My Static Site</title>
+        <meta name="description" content="This is a sample static site." />
+        <meta property="og:title" content="My Static Site" />
+        <meta
+          property="og:description"
+          content="This is a sample static site using Next.js."
+        />
+      </Head>
+      <h1>Welcome to My Static Site</h1>
+      <img src="cat.jpg" alt="Example Image" loading="lazy" />
+    </>
+  );
 }
